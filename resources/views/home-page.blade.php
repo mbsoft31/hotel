dd52880c13bd9e1da5e30e6eeb751822c61df6d8
Bekhouche Mouadh <bekhouche.mouadh@gmail.com>
2021-06-18 19:24:29 +0100

    update logo

--- resources/views/home-page.blade.php
+++ resources/views/home-page.blade.php
@@ -34,8 +34,10 @@
                                 <div class="flex items-center justify-between w-full md:w-auto">
                                     <a href="#">
                                         <span class="sr-only">Workflow</span>
-                                        <img class="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg">
+{{--                                        <img class="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg">--}}
+                                        <img src="{{asset("images/icon.png")}}" alt="logo" class="h-8 w-auto sm:h-10">
                                     </a>
+
                                     <div class="-mr-2 flex items-center md:hidden">
                                         <button x-on:click="show=!show" type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false">
                                             <span class="sr-only">Open main menu</span>
